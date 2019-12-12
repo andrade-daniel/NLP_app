bc0730d60988457c730fe76eaa1292501386ccb8
Daniel Andrade <daniel.andrade@ama.pt>
2019-12-12 23:16:22 +0000

    upd

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='abc',
+    name='yake',
     packages=find_packages(),
     url='https://github.com/LIAAD/yake',
     description='yake package installation',
     long_description=open('README.md').read(),
     install_requires=[
-        "yake==0.4.2",
+        "yake",
         ],
     dependency_links=['https://github.com/LIAAD/yake/tarball/master#egg=package-1.0'],
     include_package_data=True,
