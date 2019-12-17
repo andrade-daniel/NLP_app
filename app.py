e66387b75d58ee25ffde52b91e4db4661e5649d2
Daniel Andrade <daniel.andrade@ama.pt>
2019-12-17 11:10:24 +0000

    replace automated keyword extractor for text rank

--- app.py
+++ app.py
@@ -5,21 +5,171 @@ import spacy
 import string
 import numpy as np
 from gensim.summarization import summarize
+from spacy.lang.en.stop_words import STOP_WORDS as en_stopwords
+from spacy.lang.pt.stop_words import STOP_WORDS as pt_stopwords
+from collections import OrderedDict
 # import yake
 import plotly.express as px
 
 import re
-import nltk
+# import nltk
 import unidecode
-nltk.download('stopwords')
+# nltk.download('stopwords')
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
-nltk.download('wordnet') 
+# nltk.download('wordnet') 
 from os import path
 from PIL import Image
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+# adding non accented words in pt_stopwords
+pt_stopwords = pt_stopwords.union(set([unidecode.unidecode(k) for k in pt_stopwords]))
+
+
+# text rank class
+
+class TextRank4Keyword():
+    """Extract keywords from text"""
+    
+    def __init__(self):
+        self.d = 0.85 # damping coefficient, usually is .85
+        self.min_diff = 1e-5 # convergence threshold
+        self.steps = 10 # iteration steps
+        self.node_weight = None # save keywords and its weight
+
+    
+    def set_stopwords(self, stopwords, lang_model):  
+        """Set stop words"""
+        nlp = spacy.load(lang_model)
+        for word in stopwords:
+            lexeme = nlp.vocab[word]
+            lexeme.is_stop = True
+    
+    def sentence_segment(self, doc, candidate_pos, lower):
+        """Store those words only in cadidate_pos"""
+        sentences = []
+        for sent in doc.sents:
+            selected_words = []
+            for token in sent:
+                # Store words only with cadidate POS tag
+                if token.pos_ in candidate_pos and token.is_stop is False:
+                    if lower is True:
+                        selected_words.append(token.text.lower())
+                    else:
+                        selected_words.append(token.text)
+            sentences.append(selected_words)
+        return sentences
+        
+    def get_vocab(self, sentences):
+        """Get all tokens"""
+        vocab = OrderedDict()
+        i = 0
+        for sentence in sentences:
+            for word in sentence:
+                if word not in vocab:
+                    vocab[word] = i
+                    i += 1
+        return vocab
+    
+    def get_token_pairs(self, window_size, sentences):
+        """Build token_pairs from windows in sentences"""
+        token_pairs = list()
+        for sentence in sentences:
+            for i, word in enumerate(sentence):
+                for j in range(i+1, i+window_size):
+                    if j >= len(sentence):
+                        break
+                    pair = (word, sentence[j])
+                    if pair not in token_pairs:
+                        token_pairs.append(pair)
+        return token_pairs
+        
+    def symmetrize(self, a):
+        return a + a.T - np.diag(a.diagonal())
+    
+    def get_matrix(self, vocab, token_pairs):
+        """Get normalized matrix"""
+        # Build matrix
+        vocab_size = len(vocab)
+        g = np.zeros((vocab_size, vocab_size), dtype='float')
+        for word1, word2 in token_pairs:
+            i, j = vocab[word1], vocab[word2]
+            g[i][j] = 1
+            
+        # Get Symmeric matrix
+        g = self.symmetrize(g)
+        
+        # Normalize matrix by column
+        norm = np.sum(g, axis=0)
+        g_norm = np.divide(g, norm, where=norm!=0) # this is ignore the 0 element in norm
+        
+        return g_norm
+
+    
+    # def get_keywords(self, number=10):
+    #     """Print top number keywords"""
+    #     node_weight = OrderedDict(sorted(self.node_weight.items(), key=lambda t: t[1], reverse=True))
+    #     for i, (key, value) in enumerate(node_weight.items()):
+    #         print(key + ' - ' + str(value))
+    #         if i > number:
+    #             break
+
+    def get_keywords(self, number=10):
+        """Print top number keywords"""
+        keywords = []
+        node_weight = OrderedDict(sorted(self.node_weight.items(), key=lambda t: t[1], reverse=True))
+        for i, (key, value) in enumerate(node_weight.items()):
+            # print(key + ' - ' + str(value))
+            keywords.append(key)
+            if i > number:
+                break
+        return keywords
+        
+        
+    def analyze(self, text, lang_model,
+                candidate_pos=['NOUN', 'PROPN', 'VERB'], 
+                window_size=4, lower=False, stopwords=list()):
+        """Main function to analyze text"""
+        
+        # Set stop words
+        self.set_stopwords(stopwords, lang_model)
+        
+        # Pare text by spaCy
+        nlp = spacy.load(lang_model)
+        doc = nlp(text)
+        
+        # Filter sentences
+        sentences = self.sentence_segment(doc, candidate_pos, lower) # list of list of words
+        
+        # Build vocabulary
+        vocab = self.get_vocab(sentences)
+        
+        # Get token_pairs from windows
+        token_pairs = self.get_token_pairs(window_size, sentences)
+        
+        # Get normalized matrix
+        g = self.get_matrix(vocab, token_pairs)
+        
+        # Initionlization for weight(pagerank value)
+        pr = np.array([1] * len(vocab))
+        
+        # Iteration
+        previous_pr = 0
+        for epoch in range(self.steps):
+            pr = (1-self.d) + self.d * np.dot(g, pr)
+            if abs(previous_pr - sum(pr))  < self.min_diff:
+                break
+            else:
+                previous_pr = sum(pr)
+
+        # Get weight for each node
+        node_weight = dict()
+        for word, index in vocab.items():
+            node_weight[word] = pr[index]
+        
+        self.node_weight = node_weight
 
 
 # function to extract entities
@@ -59,13 +209,16 @@ def replace_punctuation(text):
 	return "".join(new_text_ls)
 
 @st.cache(suppress_st_warning = True)
-def clean_string(text, lang_model):
+def clean_string(text, lang_options):
 	text = str(text)
 	text = text.lower()
 	text = replace_punctuation(text)
 	text = [word for word in text.split(" ")]
 	text = [word for word in text if not any(c.isdigit() for c in word)]
-	stop_words = set(stopwords.words(lang_model))
+	if lang_options == 'EN':
+		stop_words = en_stopwords
+	else:
+		stop_words = pt_stopwords
 	text = [tok for tok in text if not tok in stop_words]
 	text = " ".join(text).strip()
 	text = unidecode.unidecode(text)
@@ -108,10 +261,8 @@ def main():
 
 		if lang_options == 'EN':
 			lang_model = 'en_core_web_sm'
-		elif lang_options == 'PT':
-			lang_model = 'pt_core_news_sm'
 		else:
-			lang_model = 'en_core_web_sm'
+			lang_model = 'pt_core_news_sm'
 
 		message = st.text_area("Enter text inside the box...")
 
@@ -176,6 +327,44 @@ def main():
 	# 			keywords = [kw for kw, res in keywords]
 				
 	# 			st.success('Keywords: ' + (', '.join(sorted(keywords))))
+
+
+# Automated Keyword Extraction
+
+	if st.sidebar.checkbox("Automated Keyword Extraction"):
+		st.subheader("Extract Keywords")
+
+		lang_options = st.selectbox("Choose language (EN/PT)",['EN','PT'])
+
+		if lang_options == 'EN':
+			stop_words = en_stopwords
+			lang_model = 'en_core_web_sm'
+		else:
+			lang_model = 'pt_core_news_sm'
+			stop_words = pt_stopwords
+
+		# nlp = spacy.load(lang_model)
+
+
+		message = st.text_area("Enter text inside the box...")
+
+		if st.button("Run"):
+			with st.spinner('Wait for it...'):
+				
+				# corpus = []
+				
+				text = ''.join([unidecode.unidecode(accented_string) for accented_string in message])
+				
+				corpus = clean_string(text, lang_options)
+
+				tr4w = TextRank4Keyword()
+				tr4w.set_stopwords(stopwords=stop_words, lang_model=lang_model)
+				# tr4w.set_stopwords(stopwords=stop_words)
+				# tr4w.analyze(ppp, candidate_pos = ['NOUN', 'PROPN', 'VERB'], window_size=4, lower=False)
+				tr4w.analyze(corpus, window_size=4, lower=False, lang_model=lang_model)
+
+				st.success('Keywords: ' + (', '.join(sorted(tr4w.get_keywords(10)))))
+
 
 
 	# Data Anonymization (erasing names)
@@ -250,11 +439,11 @@ def main():
 		lang_options = st.selectbox("Choose language (EN/PT)",['EN','PT'])
 
 		if lang_options == 'EN':
-			lang_model = 'english'
-		elif lang_options == 'PT':
-			lang_model = 'portuguese'
+			lang_model = 'en_core_web_sm'
+			stop_words = en_stopwords
 		else:
-			lang_model = 'english'
+			lang_model = 'pt_core_news_sm'
+			stop_words = pt_stopwords
 
 		message = st.text_area("Let's analyze and get some visuals...")
 		
@@ -266,8 +455,7 @@ def main():
 				
 				corpus.append(clean_string(text, lang_model))
 				
-				stop_words = set(stopwords.words(lang_model))
-
+				
 				#Word cloud
 				wordcloud = WordCloud(
 										background_color='white',
